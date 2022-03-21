cadff191e7917e09a9e9a9e34512220419ea9090
wenderson guedes <wendersonguedes6@gmail.com>
2022-03-21 14:50:31 -0300

    Null e undefined no JavaScript

---
+++ JS/data-types/null-and-undefined.js
@@ -0,0 +1,15 @@
+/*
+* undefined
+    * indefinido. não existe
+    
+* null
+    * nulo. existe, mas não tem valor
+    * objeto que não possui nada dentro
+    * diferente de indefinido
+*/
+
+console.log(undefined)
+
+console.log(null)
+
+console.log(null === undefined)
