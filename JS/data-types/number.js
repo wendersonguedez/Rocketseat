50e22948b007fbb7045ae8ad230c75134b8b3c27
wenderson guedes <wendersonguedes6@gmail.com>
2022-03-21 14:50:46 -0300

    Number no JavaScript

---
+++ JS/data-types/number.js
@@ -0,0 +1,29 @@
+/*
+    Number 
+        * Números
+
+        33, -12 // inteiros
+        12.5 // reais - float
+        NaN // Not a Number
+        Infinity // Infinito
+
+
+*/
+
+console.log(33)
+
+console.log(-312)
+
+console.log(2.4)
+
+console.log(123.6)
+
+console.log(14.6 + 9.7)
+
+console.log(19 / 2.4)
+
+console.log(19 / "abc")
+
+console.log(12.5 === Infinity)
+
+console.log(Infinity)
