6594369b2e9af88d20b60c9678aa28f6697162a9
wenderson guedes <wendersonguedes6@gmail.com>
2022-03-21 14:50:59 -0300

    Objeto em JavaScript

---
+++ JS/data-types/object.js
@@ -0,0 +1,16 @@
+/*
+* Object
+    * Objeto 
+    * Propriedades / Atributos
+    * Funcionalidades / Métodos
+
+  { propriedade: "valor"}
+
+*/
+console.log({
+    name: "Wenderson",
+    idade: 20,
+    andar: function() {
+      console.log('andar')
+    }
+  })
