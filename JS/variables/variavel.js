a2e7cf5de5c1eceaf479a8f726b669d4be6385c4
wenderson guedes <wendersonguedes6@gmail.com>
2022-03-21 14:53:31 -0300

    Praticando com variáveis

---
+++ JS/variables/variavel.js
@@ -0,0 +1,24 @@
+// var
+var clima = "Quente";
+clima = "Frio";
+
+console.log(clima);
+
+// let
+let clima2 = "Muito frio";
+clima2 = "Aqui ta muuuito frio";
+
+console.log(clima2);
+
+// const. Não pode ter seu valor alterado igual as de mais
+const clima3 = "Aqui ta muito muito quente";
+// clima3 = "Aqui até que tá muito frio".
+
+console.log(clima3);
+// -----------------------------------------------------------------------------
+
+/* Dinamismo do JS */
+let carro = true; // boolean
+carro = ""; // string
+
+console.log(typeof carro)
