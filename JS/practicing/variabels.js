4f53a69e7da84805ad1acd2ff83d35af5db38259
wenderson guedes <wendersonguedes6@gmail.com>
2022-03-21 14:52:23 -0300

    Variáveis e seus tipos no JavaScript

---
+++ JS/practicing/variabels.js
@@ -0,0 +1,29 @@
+// Variáveis e tipos de dados
+
+// declaração or declaration
+var name;
+
+// atribuição de valores or assignment
+name = "Wenderson";
+
+// que tipo de dado foi colocado na variável
+console.log(typeof name);
+
+//  declarando as variáveis uma por uma
+// let age = 20;
+// let isHuman = true;
+
+// agrupamento de declarações
+let age, isHuman;
+
+age = 20;
+isHuman = true;
+
+// multiplos argumentos na função
+console.log(name, age, isHuman);
+
+// escrita de texto + variáveis
+console.log("O " + name + " tem " + age + " anos");
+
+// interpolando valores com template literals
+console.log(`o ${name} tem ${age} anos`);
