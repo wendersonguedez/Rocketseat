2a2e5ac9b7b6b30588a913799c9b4f52a8a4e4c9
wenderson guedes <wendersonguedes6@gmail.com>
2022-03-21 14:51:29 -0300

    Praticando com objetos em JavaScript

---
+++ JS/practicing/object.js
@@ -0,0 +1,18 @@
+// Object
+
+// criando um objeto
+const person = {
+  name: "Wenderson",
+  age: 20,
+  weight: 76,
+  isAdmin: true,
+};
+
+// exibindo todos as propriedades do objeto
+console.log(person);
+
+// capturando apenas uma propriedade do objeto
+console.log(person.name);
+
+// interpolação com objetos
+console.log(`${person.name} tem ${person.age} anos`);
