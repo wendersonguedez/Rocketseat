50f8f80df54e8edc637d295d4eab02dddce3fa36
wenderson guedes <wendersonguedes6@gmail.com>
2022-03-23 11:19:27 -0300

    Iniciando com funções no JavaScript

---
+++ JS/functions/functions.js
@@ -0,0 +1,25 @@
+/*
+    As funções servem para realizar o agrupamento de código e a reutilização desse código quantas vezes forem necessárias. 
+*/
+
+// criar um aplicativo de frases motivacionais
+
+// declaration - declaração da função ou function statement
+function createPhrases() {
+  console.log("Estudar é muito bom");
+  console.log("Paciência e persistência");
+  console.log("Revisão é mãe do aprendizado");
+}
+
+/*  executar a função
+    rodar, chamar, invocar
+    execute, run, call, invoke
+*/
+createPhrases();
+createPhrases();
+
+/* Caso tente executar uma função que não foi criada, é gerado um erro de referência.
+createPhrases2();
+*/
+
+console.log("Fim do programa");
